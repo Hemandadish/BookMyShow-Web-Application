199d1749e9286f22277a7d19ca3a196b0a114f23
Hemandadish <88609112+Hemandadish@users.noreply.github.com>
2021-11-13 15:38:37 +0530

    Add files via upload

---
+++ Ticket booking/demo.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const count= document.getElementById('count');
+const total= document.getElementById('total');
+const movieSelect= document.getElementById('movie');
+
+let ticketprice = movieSelect.value;
+//update total
+
+function updateSelectedCount(){
+  const selectedSeats= document.querySelectorAll('row.seat.selected');
+
+  const selectedSeatsCount= selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketprice;
+}
+container.addEventListener('click', (e) => {
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+    e.target.classList.toggle('selected');
+  }
+
+  updateSelectedCount();
+});
